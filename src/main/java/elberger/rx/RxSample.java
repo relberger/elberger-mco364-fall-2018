33a821310b69a127d5fddcc95b1bc6159836a424
Rachel Elberger <relberge@student.touro.edu>
2018-09-13 19:49:57 -0400

    retrofit to rxjava

--- src/main/java/elberger/rx/RxSample.java
+++ src/main/java/elberger/rx/RxSample.java
@@ -1,6 +1,5 @@
 package elberger.rx;
 
-
 import com.sun.org.apache.xpath.internal.operations.Bool;
 import io.reactivex.Observable;
 import io.reactivex.Scheduler;
@@ -12,13 +11,17 @@ public class RxSample
 {
 	public static void main(String args[])
 	{
-		Observable<Integer> observable = Observable.just(5, 4, 3, 2, 1);
+		Observable<Integer> observable = Observable.just(5, 4, 3, 4, 4, 4, 3, 2, 1);
+
 
 		Disposable disposable = observable
 				.filter(integer -> integer % 2 == 0)
+				.map(integer -> integer % 2)
+				.distinct()
 				.sorted()
-				.subscribeOn(Schedulers.computation())
-				.observeOn(Schedulers.single())
+				.toList()
+				/*.subscribeOn(Schedulers.computation())
+				.observeOn(Schedulers.single())*/
 				.subscribe(System.out::println);
 
 		/*
