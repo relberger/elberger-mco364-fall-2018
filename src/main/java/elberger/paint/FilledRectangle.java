13142ad82e345422128777a690077dedf04db285
Rachel Elberger <relberger96@gmail.com>
2018-12-06 12:14:02 -0500

    filled rectangle extends rectangle

--- src/main/java/elberger/paint/FilledRectangle.java
+++ src/main/java/elberger/paint/FilledRectangle.java
@@ -2,7 +2,7 @@ package elberger.paint;
 
 import java.awt.*;
 
-public class FilledRectangle extends Shape
+public class FilledRectangle extends Rectangle
 {
 	private int width;
 	private int height;
@@ -14,86 +14,6 @@ public class FilledRectangle extends Shape
 	public FilledRectangle(int x, int y, Color color)
 	{
 		super(x, y, color);
-	}
-
-	public int getWidth()
-	{
-		return width;
-	}
-
-	public void setWidth(int width)
-	{
-		this.width = width;
-	}
-
-	public int getHeight()
-	{
-		return height;
-	}
-
-	public void setHeight(int height)
-	{
-		this.height = height;
-	}
-
-	public int getRightX()
-	{
-		return rightX;
-	}
-
-	public void setRightX(int rightX)
-	{
-		this.rightX = rightX;
-		setLeftX();
-		setWidth(Math.abs(getLeftX() - getRightX()));
-	}
-
-	public int getBottomY()
-	{
-		return bottomY;
-	}
-
-	public void setBottomY(int bottomY)
-	{
-		this.bottomY = bottomY;
-		setTopY();
-		setHeight(Math.abs(getBottomY() - getTopY()));
-	}
-
-	public int getLeftX()
-	{
-		return leftX;
-	}
-
-	public void setLeftX()
-	{
-		if (getX() <= rightX)
-		{
-			leftX = getX();
-		}
-		else
-		{
-			leftX = rightX;
-			rightX = getX();
-		}
-	}
-
-	public int getTopY()
-	{
-		return topY;
-	}
-
-	public void setTopY()
-	{
-		if (getY() <= bottomY)
-		{
-			topY = getY();
-		}
-		else
-		{
-			topY = bottomY;
-			bottomY = getY();
-		}
 	}
 
 	@Override
