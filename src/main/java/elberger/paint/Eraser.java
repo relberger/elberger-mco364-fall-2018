ec55968d20883d18785673b2d2d4acc069e6b18d
Rachel Elberger <relberger96@gmail.com>
2018-12-06 13:30:20 -0500

    changed eraser to use stroke

--- src/main/java/elberger/paint/Eraser.java
+++ src/main/java/elberger/paint/Eraser.java
@@ -25,9 +25,11 @@ public class Eraser extends Shape
 	@Override
 	public void paint(Graphics g)
 	{
-		for (Eraser eraser : erasers)
+		for (int i = 1; i < erasers.size(); i++)
 		{
-			g.fillRect(eraser.getX(), eraser.getY(), 10, 10);
+			((Graphics2D)g).setStroke(new BasicStroke(10));
+			g.drawLine(erasers.get(i).getX(), erasers.get(i).getY(),
+					erasers.get(i - 1).getX(), erasers.get(i - 1).getY());
 		}
 	}
 }
